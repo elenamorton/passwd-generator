90049b79690513b97b73671a5c903deed9ffbae6
Elena Morton <elena.uleia@gmail.com>
2017-09-30 19:25:59 +0000

    implementing password generator for uppercase chars

--- lib/passwd_gen.rb
+++ lib/passwd_gen.rb
@@ -1,8 +1,19 @@
 module Generator
+    UPPER_CHARS = ('A'..'Z').to_a
     
     def generate_password(length, uppercase=true, lowercase=true, number=true, special=true)
+        @password = []
         raise "Invalid 'length' argument: it must be higher than 0" if length <= 0
         raise "Invalid characters input: Please, include at least one chars type" unless uppercase || lowercase || number || special
+
+        @password = random_generator(UPPER_CHARS) if uppercase
+        return @password.shuffle.join[0...length]
+    end
+
+    private
+
+    def random_generator(chars)
+        chars.shuffle
     end
 
     
