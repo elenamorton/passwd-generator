4279c7d01d7ef32c9af73720615d716bb8e3b0f8
Elena Morton <elena.uleia@gmail.com>
2017-09-30 16:45:31 +0000

    adding test for invalid characters set

--- spec/passwd_gen_spec.rb
+++ spec/passwd_gen_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Generator do
     let(:generator) { Class.new { extend Generator} }
     
     context 'given an invalid set of input arguments' do
-        it 'expects to raise an error if length less or equal to 0 ' do
+        it 'expects to raise an error if length less or equal to 0' do
             expect { generator.generate_password(0) }.to raise_error "Invalid 'length' argument: it must be higher than 0"
+        end
+        
+        it 'expects to raise an error if no characters types are included' do
+            expect { generator.generate_password(1, false, false, false, false) }.to raise_error "Invalid characters input: Please, include at least one chars type."
         end
     
     end
