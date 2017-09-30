d721e550f37b5eb6da5dd264b020cead7e80f3d6
Elena Morton <elena.uleia@gmail.com>
2017-09-30 16:53:34 +0000

    implementing error raise for missing all characters sets

--- lib/passwd_gen.rb
+++ lib/passwd_gen.rb
@@ -1,7 +1,8 @@
 module Generator
     
-    def generate_password(length)
+    def generate_password(length, uppercase=true, lowercase=true, number=true, special=true)
         raise "Invalid 'length' argument: it must be higher than 0" if length <= 0
+        raise "Invalid characters input: Please, include at least one chars type" unless uppercase || lowercase || number || special
     end
 
     
