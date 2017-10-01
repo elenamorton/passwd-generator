1be842324a84d17d8ea51e5d2a52b757605d493c
Elena Morton <elena.uleia@gmail.com>
2017-10-01 17:06:27 +0000

    implementing raise error when password length is set higher than maximum allowed

--- lib/passwd_gen.rb
+++ lib/passwd_gen.rb
@@ -3,6 +3,7 @@ module Generator
     
     def generate_password(length, uppercase=true, lowercase=true, number=true, special=true)
         raise "Invalid 'length' argument: it must be higher than 0" if length <= 0
+        raise "Invalid 'length' argument: it must be lower than #{MAX_LENGTH}" if length > MAX_LENGTH
         raise "Invalid characters input: Please, include at least one chars type" unless uppercase || lowercase || number || special
         
         input = Hash.new()
